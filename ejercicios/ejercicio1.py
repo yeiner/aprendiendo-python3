5548d6fdde0eac42a2f62f5dd326d662d179dec0
Yeiner Morales <yeimor@eltiempo.com>
2019-11-20 18:09:00 -0500

    Avance curso :runner:

--- ejercicios/ejercicio1.py
+++ ejercicios/ejercicio1.py
@@ -1,4 +1,6 @@
 #/usr/bin/env python3
 
-nombre = input("Please enter your name ")
+# Escribir un programa que pregunte al usuario su nombre, y luego lo salude.
+
+nombre = input("Please enter your name: ")
 print("Hola " + nombre)
---
+++ ejercicios/ejercicio2.py
@@ -0,0 +1,10 @@
+#/usr/bin/env python3
+
+#Calcular el perímetro y área de un rectángulo dada su base y su altura.
+
+height = float(input("Enter the height of the rectangle: "))
+base =  float(input("Enter the base of the rectangle: "))
+perimeter = 2 * height * base 
+print("The perimeter of the rectangle is: ", perimeter)
+area = base * height 
+print("The area of the rectangle is: ", area) 
